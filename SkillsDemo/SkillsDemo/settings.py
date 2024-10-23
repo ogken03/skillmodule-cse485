0f6ec9ae58d3f42f8e3130f8d5a39c94a8119844
ogken03 <kennyle43@gmail.com>
2024-10-22 18:54:46 -0700

    Finished index page and css with it

--- SkillsDemo/SkillsDemo/settings.py
+++ SkillsDemo/SkillsDemo/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'SkillsDemo/static')]
